7979a5741e797a9fd5001af22237f8a408b990d1
Riwei Chen <RiweiChen@users.noreply.github.com>
2015-10-20 14:45:53 +0800

    Create ubuntu_install_easy.sh
    install some linux app.

---
+++ ubuntu_install_easy.sh
@@ -0,0 +1,18 @@
+# install cmake
+sudo apt-get install cmake
+
+# install python pip 
+sudo apt-get install python-pip
+
+# install numpy
+sudo pip install numpy
+
+# install scipy-image
+sudo pip install skimage
+
+# install scipy
+sudo pip install scipy
+
+# install fortaran compiler
+sudo apt-get install gfortran
+
